d3fa73221bb77f57a1970fd00afd505f7e9bff5a
sunbin <sunbin1@yiche.com>
2018-09-23 21:30:50 +0800

    add

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RequestMapping("/hello")
+    public String index() {
+        //System.out.println(2);
+        return "Hello World === 孙滨";
+    }
+
+
+}
